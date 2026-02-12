04f8e0220e7da6e3a1cb8aeb2dec6c7dbf358b0b
ntwenny <nathandaquinag@gmail.com>
2026-02-11 19:25:41 -0600

    Add subtract function

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ function add() {
   const result = num1 + num2;
   document.getElementById("result").textContent = "Result: " + result;
 }
+
+
+function subtract() {
+  const num1 = parseFloat(document.getElementById("num1").value);
+  const num2 = parseFloat(document.getElementById("num2").value);
+  const result = num1 - num2;
+  document.getElementById("result").textContent = "Result: " + result;
+}
